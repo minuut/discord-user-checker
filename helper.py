6ecc986d4f4f03be354a5e686dc82c3da1b20058
yon <70378641+minuut@users.noreply.github.com>
2023-06-20 21:50:33 +0200

    finished

--- helper.py
+++ helper.py
@@ -1,5 +1,6 @@
 import os
 import re
+from loguru import logger
 
 def split_lines(filename):
     file_path = os.path.join("wordlists", filename)
@@ -8,21 +9,25 @@ def split_lines(filename):
         with open(file_path, 'r') as file:
             words = file.read().split()
     except FileNotFoundError:
-        print(f"Error: File '{file_path}' not found.")
+        logger.error(f"Error: File '{file_path}' not found.")
         return
 
     if not words:
-        print(f"Error: File '{file_path}' is empty.")
+        logger.error(f"Error: File '{file_path}' is empty.")
         return
 
     filtered_words = []
     for word in words:
-        filtered_word = re.sub(r'[^a-zA-Z]', '', word)
-        filtered_words.append(filtered_word)
+        filtered_word = re.sub(r'[^a-zA-Z]', '', word)  
+        if filtered_word:
+            filtered_words.append(filtered_word)
 
     with open(file_path, 'w') as file:
         for word in filtered_words:
             file.write(word + '\n')
+
+    logger.info("Words have been split into separate lines.")
+
 
 def filter_words(filename, length):
     pattern = re.compile(r'^[a-zA-Z]{%d}$' % length)
@@ -32,7 +37,7 @@ def filter_words(filename, length):
         with open(file_path, 'r') as file:
             words = file.read().split()
     except FileNotFoundError:
-        print(f"Error: File '{file_path}' not found.")
+        logger.error(f"Error: File '{file_path}' not found.")
         return
 
     filtered_words = [word for word in words if re.match(pattern, word)]
@@ -41,6 +46,9 @@ def filter_words(filename, length):
         for word in filtered_words:
             file.write(word + '\n')
 
+    logger.info("Words have been filtered to length %d." % length)
+    
+
 def filter_words_and_save_by_length(filename):
     file_path = os.path.join("wordlists", filename)
 
@@ -48,7 +56,7 @@ def filter_words_and_save_by_length(filename):
         with open(file_path, 'r') as file:
             words = file.read().split()
     except FileNotFoundError:
-        print(f"Error: File '{file_path}' not found.")
+        logger.error(f"Error: File '{file_path}' not found.")
         return
 
     word_dict = {}
@@ -66,38 +74,35 @@ def filter_words_and_save_by_length(filename):
             for word in words:
                 file.write(word + '\n')
 
+    logger.info("Words have been filtered by length and saved to separate files.")
 
 def main():
-
-    print("""
+    logger.info("""
     Option 1: Splits words into separate lines by removing any non-alphabetic characters.
     Option 2: Filters the words in the file to a specific length input by the user.
     Option 3: Filters the words in the file by their length and saves them into separate files named according to their length 
     (i.e., 4-letter words will be saved in a file named '4char.txt', 5-letter words in '5char.txt', and so on)
-    this can be useful when you have a gigantic wordlist.
+    This can be useful when you have a gigantic wordlist.
     """)
 
     filename = input("Enter the name of the file to process: ")
 
-    print("Select an option:")
-    print("1. Split words into separate lines")
-    print("2. Filter word length in wordlist")
-    print("3. Filter word length in wordlist and save to separate files")
+    logger.info("Select an option:")
+    logger.info("1. Split words into separate lines")
+    logger.info("2. Filter words by length")
+    logger.info("3. Filter words and save by length")
 
-    option = input("Enter your choice (1, 2 or 3): ")
+    option = input("Enter your option (1/2/3): ")
 
     if option == "1":
         split_lines(filename)
-        print("Words have been split into separate lines.")
     elif option == "2":
-        length = int(input("Enter the desired word length: "))
+        length = int(input("Enter the length to filter words: "))
         filter_words(filename, length)
-        print("Words have been filtered to length %d." % length)
     elif option == "3":
         filter_words_and_save_by_length(filename)
-        print("Words have been filtered by length and saved to separate files.")
     else:
-        print("Invalid option. Please select either 1, 2 or 3.")
+        logger.error("Invalid option.")
 
 if __name__ == "__main__":
     main()
